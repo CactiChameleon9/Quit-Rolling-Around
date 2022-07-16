dca0def35017433531d76fd8e2c527d629910d9d
CactiChameleon9 <cactichameleon9@norely.localhost>
2022-07-16 12:06:46 +0100

    Add New Card Resouce Type

---
+++ Assets/CardDB/CardBD.gd
@@ -0,0 +1,56 @@
+extends Resource
+class_name CardResource
+
+enum TYPE {
+	DAMAGE
+	UTILITY
+	SPECIAL
+	EFFECT
+	MOVEMENT
+}
+
+enum EFFECT {
+	NONE
+	
+	POISON
+	BURN
+	CONFUSION
+	WEAKNESS
+	
+	SPLIT
+	DOUBLE
+	HALF
+	FLIP
+	DUPLICATE
+	REROLL
+}
+
+enum ACCEPTED_DICE {
+	DICE_1
+	DICE_2
+	DICE_3
+	DICE_4
+	DICE_5
+	DICE_6
+}
+
+export (TYPE) var type
+
+export (int) var move_amount_addition
+export (int) var move_dice_multiplyer
+
+export (int) var damage_amount_addition
+export (int) var damage_dice_multiplyer
+
+export (Array, EFFECT) var effects
+
+export (Array, ACCEPTED_DICE) var accepted_dice
+
+export (int) var number_of_dice = 1
+export (bool) var same_dice_requirement = false
+
+export (bool) var addition_dice = false
+export (int) var addition_amount = -1
+
+export (String, MULTILINE) var description = ""
+
--- UI/Card.gd
+++ UI/Card.gd
@@ -1,25 +1,20 @@
 tool
 extends Control
 
+export (Resource) var card_info
 
-enum TYPE {
-	DAMAGE = 0
-	UTILITY = 1
-	SPECIAL = 2
-	EFFECT = 3
-}
 
 const TYPE_COLORS = [
 	Color("#db4758"), # DAMAGE
 	Color("#3cc361"), # UTILITY
 	Color("#ddd55c"), # SPECIAL
 	Color("#bc5ec6"), # EFFECT
+	Color("#a4a4a4"), # MOVEMENT
 ]
 
-export (TYPE) var type
-
-
 func _ready():
+
+	# change the color of the panel to match the appropriate type
 	var card_style = $PanelContainer.get('custom_styles/panel').duplicate(true)
-	card_style.set_bg_color(TYPE_COLORS[type])
+	card_style.set_bg_color(TYPE_COLORS[card_info.type])
 	$PanelContainer.set('custom_styles/panel', card_style)
