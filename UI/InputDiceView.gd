e84ea6786cf35048457f35db7b4e24cccfc748ff
CactiChameleon9 <cactichameleon9@norely.localhost>
2022-07-25 20:23:33 +0100

    Better support Extra Info

--- UI/InputDiceView.gd
+++ UI/InputDiceView.gd
@@ -1,9 +1,14 @@
 tool
 extends Control
 
+
 func _process(_delta):
 	
 	# update the pivot offset to make sure the object's animations
 	# are always centered 
 	$Sprite.rect_pivot_offset = rect_size/2
 	$Particles2D.position = rect_size/2
+
+
+func set_extra_info(text : String):
+	$"%ExtraInfo".text = text
